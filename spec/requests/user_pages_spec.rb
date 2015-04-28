d2531ac2de4fe61b842199d7c358cdc0c0624bd2
uzuyh <miller_sphere@yahoo.co.jp>
2015-04-29 05:32:33 +0900

    Add user following

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -66,95 +66,173 @@ describe "UserPages" do
       it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
     end
-  end                       
 
-  describe "signup page" do
-    before { visit signup_path }
+    describe "signup page" do
+      before { visit signup_path }
 
-    it { should have_content('Sigh up') }
-    it { should have_title(full_title('Sigh up')) }
-  end
+      it { should have_content('Sigh up') }
+      it { should have_title(full_title('Sigh up')) }
+    end
 
-  describe "signup" do
+    describe "signup" do
 
-    before { visit signup_path }
+      before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+      let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+      describe "with invalid information" do
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+      end
+
+      describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: 'user@example.com') }
+
+          it { should have_link('Sign out') }
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
       end
     end
 
-    describe "with valid information" do
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
+    describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        sign_in user
+        visit edit_user_path(user)
       end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+      describe "page" do
+        it { should have_content("Update your profile") }
+        it { should have_title("Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
       end
 
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+      describe "with invalid information" do
+        before { click_button "Save changes" }
 
-        it { should have_link('Sign out') }
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
-
-  describe "edit" do
-    let(:user) { FactoryGirl.create(:user) }
-    before do
-      sign_in user
-      visit edit_user_path(user)
-    end
-
-    describe "page" do
-      it { should have_content("Update your profile") }
-      it { should have_title("Edit user") }
-      it { should have_link('change', href: 'http://gravatar.com/emails') }
-    end
-
-    describe "with invalid information" do
-      before { click_button "Save changes" }
-
-      it { should have_content('error') }
-    end
-
-    describe "with valid information" do
-      let(:new_name)  { "New Name" }
-      let(:new_email) { "new@example.com" }
-      before do
-        fill_in "Name",             with: new_name
-        fill_in "Email",            with: new_email
-        fill_in "Password",         with: user.password
-        fill_in "Confirm Password", with: user.password
-        click_button "Save changes"
+        it { should have_content('error') }
       end
 
-      it { should have_title(new_name) }
-      it { should have_selector('div.alert.alert-success') }
-      it { should have_link('Sign out', href: signout_path) }
-      specify { expect(user.reload.name).to  eq new_name }
-      specify { expect(user.reload.email).to eq new_email }
+      describe "with valid information" do
+        let(:new_name)  { "New Name" }
+        let(:new_email) { "new@example.com" }
+        before do
+          fill_in "Name",             with: new_name
+          fill_in "Email",            with: new_email
+          fill_in "Password",         with: user.password
+          fill_in "Confirm Password", with: user.password
+          click_button "Save changes"
+        end
+
+        it { should have_title(new_name) }
+        it { should have_selector('div.alert.alert-success') }
+        it { should have_link('Sign out', href: signout_path) }
+        specify { expect(user.reload.name).to  eq new_name }
+        specify { expect(user.reload.email).to eq new_email }
+      end
     end
+
+    describe "following/followers" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { user.follow!(other_user) }
+
+      describe "followed users" do
+        before do
+          sign_in user
+          visit following_user_path(user)
+        end
+
+        it { should have_title(full_title('Following')) }
+        it { should have_selector('h3', text: 'Following') }
+        it { should have_link(other_user.name, href: user_path(other_user)) }
+      end
+
+      describe "followers" do
+        before do
+          sign_in other_user
+          visit followers_user_path(other_user)
+        end
+
+        it { should have_title(full_title('Followers')) }
+        it { should have_selector('h3', text: 'Followers') }
+        it { should have_link(user.name, href: user_path(user)) }
+      end
+    end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
+    end                       
   end
 end
